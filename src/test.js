d91fc955f83ddbf15183e67d5aa7267979e81fb4
Charly J <janoncarlos@gmail.com>
2024-11-21 13:40:01 -0300

    groq test ok

--- src/test.js
+++ src/test.js
@@ -1,7 +1,7 @@
 import { groqLLM } from './config.js';
 
 async function testGroq() {
-    const response = await groqLLM.invoke("What's the current status of the connection?");
+    const response = await groqLLM.invoke("What's the current status of the Argentinian politics ?");
     console.log(response);
 }
 
